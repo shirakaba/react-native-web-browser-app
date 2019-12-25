4a44a8bd79940b471231b351ffe07d9c8bee9132
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2019-12-25 22:46:04 +0000

    Undo manual linking:
    error React Native CLI uses autolinking for native dependencies, but the following modules are linked manually:
      - react-native-gesture-handler (to unlink run: "react-native unlink react-native-gesture-handler")
      - react-native-reanimated (to unlink run: "react-native unlink react-native-reanimated")
      - react-native-screens (to unlink run: "react-native unlink react-native-screens")

--- android/app/src/main/java/com/linguabrowse/MainApplication.java
+++ android/app/src/main/java/com/linguabrowse/MainApplication.java
@@ -8,9 +8,6 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.linguabrowse.generated.BasePackageList;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
-import com.swmansion.reanimated.ReanimatedPackage;
-import com.swmansion.rnscreens.RNScreensPackage;
 
 import org.unimodules.adapters.react.ReactAdapterPackage;
 import org.unimodules.adapters.react.ModuleRegistryAdapter;
