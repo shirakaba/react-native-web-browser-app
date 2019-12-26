6004e1a67fa02c88dc35a7aed3deb0852c6176e2
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2019-12-26 15:55:57 +0000

    "~" import now resolving

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,17 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["."],
+          extensions: [".tsx", ".ts", ".jsx", ".js"],
+          alias: {
+            "~": "./src",
+          }
+        }
+      ]
+    ],
   };
 };
