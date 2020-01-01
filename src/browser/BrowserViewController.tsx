3ccbdac4b572a1f44c7c99e5aa7bcaec25d7d458
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2020-01-01 16:13:43 +0000

    Revert "Proof-of-concept for only activating retraction upon a significant velocity. Not quite working."
    This reverts commit 396cb5c97d2a1745a4bc1affbab4b87fc6fe65cc.

--- src/browser/BrowserViewController.tsx
+++ src/browser/BrowserViewController.tsx
@@ -325,20 +325,10 @@ class WebViewContainer extends React.Component<WebViewContainerProps & ViewProps
                                         y: (y) => {
                                             return Animated.block([
                                                 Animated.cond(
-                                                    Animated.and(
-                                                        /* We won't update scrollY if there was no panGesture movement.
-                                                         * This is necessary because onScroll is called without gestures
-                                                         * sometimes, e.g. due to autolayout when first initialising. */
-                                                        Animated.neq(y, 0),
-                                                        /* TODO: We won't update scrollY unless the gesture velocity was significant.
-                                                         * This approach isn't working because the onScrollEndDrag event comes after
-                                                         * the onScroll event, so we're always one step out of date. Not sure how iOS
-                                                         * Safari does it, as they'll be up against the same issue. */
-                                                        Animated.greaterOrEq(
-                                                            Animated.abs(this.props.scrollEndDragVelocity),
-                                                            0.5
-                                                        ),
-                                                    ),
+                                                    /* We won't update scrollY if there was no panGesture movement.
+                                                     * This is necessary because onScroll is called without gestures
+                                                     * sometimes, e.g. due to autolayout when first initialising. */
+                                                    Animated.neq(y, 0),
                                                        
                                                     /* We always receive a gesture relative to 0.
                                                     * e.g. when panning down (scrolling up): +3, 9, 12, 20.
@@ -360,7 +350,7 @@ class WebViewContainer extends React.Component<WebViewContainerProps & ViewProps
                                                 Animated.call(
                                                     [y],
                                                     (r) => {
-                                                        console.log(`panGestureTranslationInWebView`, r[0]);
+                                                        console.log(`Reanimated got arg`, r[0]);
                                                     }
                                                 )
                                             ]);
@@ -378,17 +368,7 @@ class WebViewContainer extends React.Component<WebViewContainerProps & ViewProps
                             {
                                 nativeEvent: {
                                     velocity: {
-                                        y: (y) => {
-                                            return Animated.block([
-                                                Animated.set(this.props.scrollEndDragVelocity, y),
-                                                Animated.call(
-                                                    [y],
-                                                    (r) => {
-                                                        console.log(`velocity`, r[0]);
-                                                    }
-                                                )
-                                            ]);
-                                        }
+                                        y: this.props.scrollEndDragVelocity
                                     }
                                 }
                             }
